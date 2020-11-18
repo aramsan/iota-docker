b84a47efc64290e8e7ae0f3367e19079b440a7ba
Aram Mine <aram.mine@gaiax.com>
2020-11-18 15:04:01 +0900

    ハッシュの管理方法を変更

--- test_script/continuous_writing_formated_data.js
+++ test_script/continuous_writing_formated_data.js
@@ -28,8 +28,9 @@ var seq_id = 0;
 var frame_id = 0;
 var camera_id = 1;
 var start_frame_mumber = 1;
-var hash = "0";
-var previous_transaction_hash = hash;
+var previous_temporary_transction_hash = 0;
+var previous_transaction_hash = 0;
+
 
 var keypair = createKeyPair()
 console.log(keypair);
@@ -42,33 +43,37 @@ console.log(verifySignature(hash, keypair.publicKey, signature ));
 setInterval(function () {
     counter++;
     frame_id++;
-    let previous_hash = hash;
     let current_hash = createDataHash();
-    hash = StackHash(previous_hash, current_hash);
-    /*
+    let temporary_transction_hash = StackHash(previous_temporary_transction_hash, current_hash);
+    //*
     console.log("---------------------");
     console.log(counter);
-    console.log(hash);
-    */
-    if ((counter % 10) == 0) {
+    console.log(previous_temporary_transction_hash);
+    console.log(current_hash);
+    console.log(temporary_transction_hash);
+    console.log("---------------------");
+    //*/
+    if ((counter % 3) == 0) {
+        let hash = temporary_transction_hash;
         seq_id++;
         let signature = createSignature(hash, keypair.privateKey );
         let data = {
             "seq_id": seq_id,
             "camera_id": camera_id,
-            "start_frame_number": start_frame_mumber,
-            "end_frame_number": counter,
+            "first_frame_number": start_frame_mumber,
+            "last_frame_number": counter,
             "previous_transaction_hash": previous_transaction_hash, 
             "hash": hash,
             "signature": signature,
             "camera_public_key": keypair.publicKey
         };
         console.log(data);
-        writeToTangle({"node": iota, "address":address, "data": data});
+        //writeToTangle({"node": iota, "address":address, "data": data});
+        // 次の記録のための各パラメーターの準備
         start_frame_mumber = counter + 1;
-        previous_transaction_hash = hash;
-        hash = 0; // 次のハッシュチェーンの初期値をゼロにする
+        previous_transaction_hash = hash; // 今回登録したハッシュが次のトランザクションで使う1個前のハッシュ値になる
     }
+    previous_temporary_transction_hash = temporary_transction_hash;// トランザクション最初のハッシュ値は今のフレームのみのハッシュ値を使う
 }, 33);
 
 function StackHash(previous_hash, current_hash) {
